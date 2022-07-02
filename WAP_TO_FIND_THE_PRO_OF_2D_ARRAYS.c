158bb9a111937a857304d102a5e2b4d9b405f02b
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2022-07-02 22:49:00 +0545

    Add files via upload

---
+++ WAP_TO_FIND_THE_PRO_OF_2D_ARRAYS.c
@@ -0,0 +1,25 @@
+int main(){
+	int a[50][50],i,j,size,row1,col1,pro=1;
+	
+	printf("Enter the size of the row: ");
+	scanf("%d",&row1);
+	
+	printf("Enter the size of the col: ");
+	scanf("%d",&col1);
+	
+	for(i=0;i<=row1-1;i++){
+		for(j=0;j<=col1-1;j++){
+			printf("\nEnter the values in the array a[%d][%d]: ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	printf("The array is: \n");
+	for(i=0;i<=row1-1;i++){
+		for(j=0;j<=col1-1;j++){
+			printf("a[%d][%d]=%d\t",i,j,3*a[i][j]);
+		}
+		printf("\n");
+	}	
+	return 0;
+}
