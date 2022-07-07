56b89ad6ed17031114ae9eb8737c8b5a11c048f8
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2022-07-07 16:57:49 +0545

    Add files via upload

---
+++ WAP_TO_ADD_2D_ARRAY_WITH_FUN.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+void input(int a[30][30],int row,int col);
+void display(int b[30][30],int row,int col);
+void sum(int a[30][30],int b[30][30],int row,int col);
+int i,j;
+
+int main(){
+	int arrayA[30][30],arrayB[30][30],row,col;
+	printf("Enter the size of the row and col: ");
+	scanf("%d %d",&row,&col);
+	input(arrayA,row,col);
+	input(arrayB,row,col);
+	display(arrayA,row,col);
+	display(arrayB,row,col);
+	sum(arrayA,arrayB,row,col);
+  return 0;
+}
+
+void input(int a[30][30],int row,int col)
+{
+	printf("\nEnter the value in the matrix:\n");
+	for(i=0;i<=row-1;i++)
+	{
+	for(j=0;j<=col-1;j++)
+	{
+		printf("\na[%d][%d]=",i,j);
+		scanf("%d",&a[i][j]);
+	}	
+	}
+}
+
+void display(int b[30][30],int row,int col)
+{
+	printf("\nThe values in the matrix is: \n");
+	for(i=0;i<=row-1;i++)
+	{
+		for(j=0;j<=col-1;j++)
+		{
+		printf("a[%d][%d]=%d\t",i,j,b[i][j]);	
+		}
+		printf("\n");
+	}
+}
+
+void sum(int a[30][30],int b[30][30],int row,int col)
+{
+	int sum[30][30];
+	printf("\nThe sum of the two arrays are: \n");
+	for(i=0;i<=row-1;i++)
+	{
+		for(j=0;j<=col-1;j++)
+		{
+		sum[i][j]=a[i][j]+b[i][j];	
+		}
+	}
+	for(i=0;i<=row-1;i++)
+	{
+		for(j=0;j<=col-1;j++)
+		{
+		printf("sum[%d][%d]=%d\t",i,j,sum[i][j]);	
+		}
+		printf("\n");
+	}	
+}
